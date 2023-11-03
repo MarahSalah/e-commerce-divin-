c3ad30c854091f56614c089e9c88f118c9439f74
Marah Salah <marahsalah9@gmail.com>
2023-11-03 21:55:13 +0300

    new

--- views/view/src/App.js
+++ views/view/src/App.js
@@ -1,31 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { Footer } from './components/Footer';
-import { Home } from './pages/Home';
+import {Home} from './pages/Home';
 import { ProductDetails } from './pages/ProductDetails';
 import { ShopAll } from './pages/ShopAll';
 import { ShoppingCart } from './pages/ShoppingCart';
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import SignUp  from './pages/SignUp';
 
 function App() {
   return (
-    <>
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/product/:id' element={<ProductDetails />}/>
-        <Route path='/shopall' element={<ShopAll />}/>
-        <Route path='/shoppingcart' element={<ShoppingCart />}/>
-        <Route path='/signin' element={<SignIn />}/>
-        <Route path='/signup' element={<SignUp />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/shopall' element={<ShopAll />} />
+        <Route path='/shoppingcart' element={<ShoppingCart />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
       </Routes>
-    <Footer />
+      <Footer />
     </BrowserRouter>
-    </>
   );
 }
 
